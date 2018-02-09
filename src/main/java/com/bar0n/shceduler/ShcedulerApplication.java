91189d97bf25f56a4cf89ed92de737828ca799a0
baron <dbaron@intersog.com>
2018-02-09 08:26:58 +0200

    init

---
+++ src/main/java/com/bar0n/shceduler/ShcedulerApplication.java
@@ -0,0 +1,12 @@
+package com.bar0n.shceduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShcedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShcedulerApplication.class, args);
+	}
+}
