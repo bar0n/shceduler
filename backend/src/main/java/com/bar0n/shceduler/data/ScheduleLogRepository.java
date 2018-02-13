4e67a5f21007b69c063a3b578ce72a699fdbdd58
dbaron <dbaron@intersog.com>
2018-02-13 14:20:32 +0200

    added delete cascade

--- backend/src/main/java/com/bar0n/shceduler/data/ScheduleLogRepository.java
+++ backend/src/main/java/com/bar0n/shceduler/data/ScheduleLogRepository.java
@@ -4,10 +4,13 @@ import com.bar0n.shceduler.model.ScheduleLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -17,4 +20,8 @@ import java.util.List;
 @Repository
 public interface ScheduleLogRepository extends JpaRepository<ScheduleLog, Long> {
     Page<ScheduleLog> findAllByOrderByIdDesc(Pageable pageable);
+    @Modifying
+    @Transactional
+    @Query("delete from ScheduleLog u where u.schedule.id = ?1")
+    void deleteScheduleLogsByscheduleId(Long id);
 }
--- backend/src/main/java/com/bar0n/shceduler/rest/ScheduleResource.java
+++ backend/src/main/java/com/bar0n/shceduler/rest/ScheduleResource.java
@@ -1,6 +1,7 @@
 package com.bar0n.shceduler.rest;
 
 
+import com.bar0n.shceduler.data.ScheduleLogRepository;
 import com.bar0n.shceduler.data.ScheduleRepository;
 import com.bar0n.shceduler.model.Schedule;
 import com.bar0n.shceduler.rest.errors.BadRequestAlertException;
@@ -32,10 +33,12 @@ public class ScheduleResource {
     private static final String ENTITY_NAME = "schedule";
 
     private final ScheduleRepository scheduleRepository;
+    private final ScheduleLogRepository scheduleLogRepository;
     private final ScheduleService scheduleService;
 
-    public ScheduleResource(ScheduleRepository scheduleRepository, ScheduleService scheduleService) {
+    public ScheduleResource(ScheduleRepository scheduleRepository, ScheduleLogRepository scheduleLogRepository, ScheduleService scheduleService) {
         this.scheduleRepository = scheduleRepository;
+        this.scheduleLogRepository = scheduleLogRepository;
         this.scheduleService = scheduleService;
     }
 
@@ -47,7 +50,6 @@ public class ScheduleResource {
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PostMapping("/schedules")
-  
     public ResponseEntity<Schedule> createSchedule(@RequestBody Schedule schedule) throws URISyntaxException {
         log.debug("REST request to save Schedule : {}", schedule);
         if (schedule.getId() != null) {
@@ -72,7 +74,6 @@ public class ScheduleResource {
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PutMapping("/schedules")
-  
     public ResponseEntity<Schedule> updateSchedule(@RequestBody Schedule schedule) throws URISyntaxException {
         log.debug("REST request to update Schedule : {}", schedule);
         if (schedule.getId() == null) {
@@ -94,7 +95,6 @@ public class ScheduleResource {
      * @return the ResponseEntity with status 200 (OK) and the list of schedules in body
      */
     @GetMapping("/schedules")
-  
     public ResponseEntity<List<Schedule>> getAllSchedules(Pageable pageable) {
         log.debug("REST request to get a page of Schedules");
         Page<Schedule> page = scheduleRepository.findAll(pageable);
@@ -109,7 +109,6 @@ public class ScheduleResource {
      * @return the ResponseEntity with status 200 (OK) and with body the schedule, or with status 404 (Not Found)
      */
     @GetMapping("/schedules/{id}")
-  
     public ResponseEntity<Schedule> getSchedule(@PathVariable Long id) {
         log.debug("REST request to get Schedule : {}", id);
         Optional<Schedule> byId = scheduleRepository.findById(id);
@@ -123,116 +122,13 @@ public class ScheduleResource {
      * @return the ResponseEntity with status 200 (OK)
      */
     @DeleteMapping("/schedules/{id}")
-  
     public ResponseEntity<Void> deleteSchedule(@PathVariable Long id) {
         log.debug("REST request to delete Schedule : {}", id);
         Optional<Schedule> byId = scheduleRepository.findById(id);
+        scheduleLogRepository.deleteScheduleLogsByscheduleId(id);
         byId.ifPresent(scheduleRepository::delete);
-
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
     }
- /*   private static final Logger logger = LoggerFactory.getLogger(RestApiController.class);
 
-    private final ScheduleService scheduleService; //Service which will do all data retrieval/manipulation work
-
-    @Autowired
-    public RestApiController(ScheduleService scheduleService) {
-        this.scheduleService = scheduleService;
-    }
-
-    // -------------------Retrieve All Schedules---------------------------------------------
-
-    @RequestMapping(value = "/schedule2/", method = RequestMethod.GET)
-    public ResponseEntity listAllSchedules() {
-        List<Schedule> schedules = scheduleService.findAllSchedules();
-        if (schedules.isEmpty()) {
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-            // You many decide to return HttpStatus.NOT_FOUND
-        }
-        return new ResponseEntity<>(schedules, HttpStatus.OK);
-    }
-
-    // -------------------Retrieve Single Schedule------------------------------------------
-
-    @RequestMapping(value = "/schedule2/{id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getSchedule(@PathVariable("id") long id) {
-        logger.info("Fetching Schedule with id {}", id);
-        Schedule schedule = scheduleService.findById(id);
-        if (schedule == null) {
-            logger.error("Schedule with id {} not found.", id);
-            return new ResponseEntity(new CustomErrorType("Schedule with id " + id
-                    + " not found"), HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<>(schedule, HttpStatus.OK);
-    }
-
-    // -------------------Create a Schedule-------------------------------------------
-
-    @RequestMapping(value = "/schedule2/", method = RequestMethod.POST)
-    public ResponseEntity<?> createSchedule(@RequestBody Schedule schedule, UriComponentsBuilder ucBuilder) {
-        logger.info("Creating Schedule : {}", schedule);
-
-        if (scheduleService.isScheduleExist(schedule)) {
-            logger.error("Unable to create. A Schedule with name {} already exist", schedule.getName());
-            return new ResponseEntity(new CustomErrorType("Unable to create. A Schedule with name " +
-
-                    schedule.getName() + " already exist."), HttpStatus.CONFLICT);
-        }
-        scheduleService.saveSchedule(schedule);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/api/schedule2/{id}").buildAndExpand(schedule.getId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
-    }
-
-    // ------------------- Update a Schedule ------------------------------------------------
-
-    @RequestMapping(value = "/schedule2/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<?> updateSchedule(@PathVariable("id") long id, @RequestBody Schedule schedule) {
-        logger.info("Updating Schedule with id {}", id);
-
-        Schedule currentSchedule = scheduleService.findById(id);
-
-        if (currentSchedule == null) {
-            logger.error("Unable to update. Schedule with id {} not found.", id);
-            return new ResponseEntity(new CustomErrorType("Unable to upate. Schedule with id " + id + " not found."),
-                    HttpStatus.NOT_FOUND);
-        }
-
-        currentSchedule.setName(schedule.getName());
-        currentSchedule.setCron(schedule.getCron());
-        currentSchedule.setNext(schedule.getNext());
-        currentSchedule.setStart(schedule.getStart());
-
-
-        scheduleService.updateSchedule(currentSchedule);
-        return new ResponseEntity<>(currentSchedule, HttpStatus.OK);
-    }
-
-    // ------------------- Delete a Schedule-----------------------------------------
-
-    @RequestMapping(value = "/schedule2/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<?> deleteSchedule(@PathVariable("id") long id) {
-        logger.info("Fetching & Deleting Schedule with id {}", id);
-
-        Schedule schedule = scheduleService.findById(id);
-        if (schedule == null) {
-            logger.error("Unable to delete. Schedule with id {} not found.", id);
-            return new ResponseEntity(new CustomErrorType("Unable to delete. Schedule with id " + id + " not found."),
-                    HttpStatus.NOT_FOUND);
-        }
-        scheduleService.deleteScheduleById(id);
-        return new ResponseEntity<Schedule>(HttpStatus.NO_CONTENT);
-    }
-
-    // ------------------- Delete All Schedules-----------------------------
-
-    @RequestMapping(value = "/schedule2/", method = RequestMethod.DELETE)
-    public ResponseEntity<Schedule> deleteAllSchedules() {
-        logger.info("Deleting All Schedules");
-
-        scheduleService.deleteAllSchedules();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }*/
 
 }
